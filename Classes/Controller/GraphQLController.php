d8eaf39a877889d984c901660833f8894ed3d527
Simon Gadient <simon.gadient@oniva.events>
2026-03-10 11:42:39 +0100

    Persisted query support

--- Classes/Controller/GraphQLController.php
+++ Classes/Controller/GraphQLController.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Oniva\GraphQL\Controller;
 
+use GraphQL\Error\SyntaxError;
 use GraphQL\GraphQL;
+use GraphQL\Language\AST\DocumentNode;
+use GraphQL\Language\Parser;
+use GraphQL\Language\Source;
+use GraphQL\Validator\DocumentValidator;
+use GraphQL\Validator\Rules\QueryComplexity;
+use Neos\Cache\Frontend\FrontendInterface;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\Controller\ActionController;
 use Oniva\GraphQL\Context;
@@ -26,6 +33,13 @@ class GraphQLController extends ActionController
     /**
      * @Flow\Inject
      *
+     * @var FrontendInterface
+     */
+    protected $persistedQueryCache;
+
+    /**
+     * @Flow\Inject
+     *
      * @var ValidationRuleService
      */
     protected $validationRuleService;
@@ -36,6 +50,13 @@ class GraphQLController extends ActionController
      * @var string
      */
     protected $contextClassName;
+
+    /**
+     * @Flow\InjectConfiguration("cacheNonPersistedQueries")
+     *
+     * @var bool
+     */
+    protected $cacheNonPersistedQueries;
 
     /**
      * @Flow\InjectConfiguration("endpoints")
@@ -64,6 +85,7 @@ class GraphQLController extends ActionController
      * @Flow\SkipCsrfProtection
      *
      * @param array|null $variables
+     * @param array|null $extensions
      *
      * @throws \Neos\Flow\Mvc\Exception\NoSuchArgumentException
      * @throws InvalidContextException
@@ -71,14 +93,70 @@ class GraphQLController extends ActionController
      * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingTraversableTypeHintSpecification
      * @phpcsSuppress PEAR.Commenting.FunctionComment.MissingParamTag
      */
-    public function queryAction(string $endpoint, string $query, ?array $variables = null, ?string $operationName = null): string
+    public function queryAction(string $endpoint, ?string $query = null, ?array $variables = null, ?string $operationName = null, ?array $extensions = null): string
     {
         if ($variables !== null && is_string($this->request->getArgument('variables'))) {
             $variables = json_decode($this->request->getArgument('variables'), true);
         }
 
+        $persistedQueryHash = $extensions['persistedQuery']['sha256Hash'] ?? null;
+        $persistedQuery = is_string($persistedQueryHash);
+        if ($query === null && $persistedQuery === false) {
+            $this->response->setContentType('application/json');
+            return json_encode(['errors' => [['message' => 'Must provide query string.']]]);
+        }
+
         $schema = $this->schemaService->getSchemaForEndpoint($endpoint);
         $validationRules = $this->validationRuleService->getValidationRulesForEndpoint($endpoint);
+
+        $documentNode = null;
+        if ($persistedQuery || $this->cacheNonPersistedQueries) {
+            $cacheKey = $persistedQuery ? $persistedQueryHash : hash('sha256', $query);
+            $documentNode = $this->persistedQueryCache->get($cacheKey);
+
+            $validationErrorCount = 0;
+            if ($documentNode instanceof DocumentNode === false) {
+                if ($persistedQuery) {
+                    if ($query === null) {
+                        $this->response->setContentType('application/json');
+                        return json_encode([
+                            'errors' => [[
+                                'message' => 'PersistedQueryNotFound',
+                                'extensions' => ['code' => 'PERSISTED_QUERY_NOT_FOUND'],
+                            ]],
+                        ]);
+                    } elseif (hash('sha256', $query) !== $persistedQueryHash) {
+                        $this->response->setContentType('application/json');
+                        return json_encode([
+                            'errors' => [[
+                                'message' => 'Provided sha256Hash does not match query',
+                                'extensions' => ['code' => 'PERSISTED_QUERY_HASH_MISMATCH'],
+                            ]],
+                        ]);
+                    }
+                }
+
+                try {
+                    $documentNode = Parser::parse(new Source($query, 'GraphQL'));
+                } catch (SyntaxError) {
+                    $documentNode = null;
+                }
+
+                if ($documentNode !== null) {
+                    // Validate the query against the schema before writing the cache.
+                    $cachableRules = $this->filterCachableRules($validationRules, true);
+                    $validationErrorCount = count(DocumentValidator::validate($schema, $documentNode, $cachableRules));
+                    if ($validationErrorCount === 0) {
+                        $this->persistedQueryCache->set($cacheKey, $documentNode);
+                    }
+                }
+            }
+
+            // If there were no validation errors of the cachable rules, we do not need to validate them again
+            if ($validationErrorCount === 0) {
+                $validationRules = $this->filterCachableRules($validationRules, false);
+            }
+        }
 
         $endpointConfiguration = $this->endpointConfigurations[$endpoint] ?? [];
 
@@ -94,14 +172,14 @@ class GraphQLController extends ActionController
         }
 
         if (isset($endpointConfiguration['logRequests']) && $endpointConfiguration['logRequests'] === true) {
-            $this->requestLogger->info('Incoming graphql request', ['endpoint' => $endpoint, 'query' => json_encode($query), 'variables' => empty($variables) ? 'none' : $variables]);
+            $this->requestLogger->info('Incoming graphql request', ['endpoint' => $endpoint, 'query' => $persistedQuery ? $persistedQueryHash : json_encode($query), 'variables' => empty($variables) ? 'none' : $variables]);
         }
 
         GraphQL::setDefaultFieldResolver([DefaultFieldResolver::class, 'resolve']);
 
         $result  = GraphQL::executeQuery(
             $schema,
-            $query,
+            $documentNode instanceof DocumentNode ? $documentNode : $query,
             null,
             $context,
             $variables,
@@ -112,5 +190,16 @@ class GraphQLController extends ActionController
 
         $this->response->setContentType('application/json');
         return json_encode($result->toArray());
+    }
+
+    /**
+     * The QueryComplexity validation rule is not cachable, so we need to filter it out
+     * @see GraphQL::promiseToExecute()
+     */
+    protected function filterCachableRules(array $validationRules, bool $cachable): array
+    {
+        return array_filter($validationRules, function ($rule) use ($cachable) {
+            return $rule instanceof QueryComplexity === !$cachable;
+        });
     }
 }
